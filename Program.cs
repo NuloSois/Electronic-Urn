8363a31f218cb1210696087a39f5905c260e9bbe
NuloSois <nulosoisdavi@gmail.com>
2022-11-07 13:15:22 -0300

    Reworked the program

--- Program.cs
+++ Program.cs
@@ -1,69 +1,113 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("This program simulates various voting systems, including an audit system for checking the results");
+Console.WriteLine("If it's your first time, write ''help'' in order to know the commands");
 
-Console.WriteLine("Insira o Numero de Eleitores");
-int Eleitores = Convert.ToInt32(Console.ReadLine() ?? "1");
+string initInput = Console.ReadLine() ?? "help";
 
+if (initInput == "help")
+{
+    Console.WriteLine(@"Command Sheet (main):
+    ________________________________________
+    help      | opens menu
+    majority  | opens majority voting system
+    plurality | opens plurality voting system
+    ________________________________________");
 
-int votosBonoro = 0;
-int votosLula = 0;
-int votosCiro = 0;
-int votosTebet = 0;
-int votosNulo = 0;
+    Console.WriteLine(@"Command Sheet (suffixes):
+    ____________________________________________
+    -single | makes the voting for a single seat
+    -many   | makes the voting for many seats
+    ____________________________________________");
 
-for (int i = 0; i < Eleitores; i++)
+    Console.WriteLine(@"Example:
+    
+    majority -single
+    
+    (this will open the program for a majority election for a single seat)");
+}
+
+else
+
+{
+
+    Console.WriteLine("How many candidates will be up for the seat ?");
+    int candidates = 2;
+    candidates = Convert.ToInt16(Console.ReadLine());
+
+    List<string> CandidatesName   = new List<string>{};
+    List<int> CandidatesNumber    = new List<int>{};
+    List<long> CandidatesVotes    = new List<long>{};
+    CandidatesName.Add("Nulo");
+    CandidatesNumber.Add(0);
+    string Name = "";
+    int Number  = 0;
+
+    for (int i = 0; i < candidates; i++)
     {
-    Console.WriteLine("Insira seu candidato");
-    string candidato =  Console.ReadLine() ?? "A";
-    switch(candidato)
+        Console.WriteLine("Write the name of the candidate");
+            Name = Console.ReadLine() ?? $"Generic {i}";
+            CandidatesName.Add(Name);
+        Console.WriteLine("Write the number of said candidate");
+            Number = Convert.ToInt16(Console.ReadLine());
+            CandidatesNumber.Add(Number);
+
+            CandidatesVotes.Add(0);
+    }
+
+    Console.WriteLine("Insert the number of voters");
+    int Eleitores = Convert.ToInt32(Console.ReadLine() ?? "1");
+
+    for (int i = 0; i <= Eleitores; i++)
         {
-            case "13" or "Lula":
-            votosLula = votosLula++;
-            break;
 
-            case "22" or "Bonoro":
-            votosBonoro = votosBonoro++;
-            break;
+        Console.WriteLine("Insert your candidate's Number");
+        int candidato = -1;
+         candidato =  Convert.ToInt16(Console.ReadLine());
 
-            case "12" or "Ciro":
-            votosCiro = votosCiro++;
-            break;
+        if (candidato == -1)
+            {
+                Console.WriteLine("Invalid Candidate");
+                i = i--;
+                break;
+            }
 
-            case "25" or "Tebet":
-            votosTebet = votosTebet++;
-            break;
+        if (candidato != -1) 
+            {
+                for(int inc = 1; inc < CandidatesNumber.Count(); inc++)
+                {
+                    if (candidato == CandidatesNumber[inc])
+                    {
+                        CandidatesVotes[inc]++;
+                    }
+                }
+            }
 
-            case "Nulo":
-            votosNulo = votosNulo++;
-            break;
+            Console.WriteLine(string.Join("/", CandidatesName));
+            Console.WriteLine(string.Join("/", CandidatesNumber));
+            Console.WriteLine(string.Join("/", CandidatesVotes));
 
-            default:
-            Console.WriteLine("Candidato Invalido");
-            i = i--;
-            break;
         }
-    }
+//Audit System in Progress
 
-    Console.WriteLine(@$"Votos:
-    Lula: {votosLula}
-    Bolsonaro: {votosBonoro}
-    Ciro: {votosCiro}
-    Tebet: {votosTebet}
-    ");
+/*
+        Console.WriteLine("Você quer auditar os resultados ? (S/N) ");
 
-    Console.WriteLine("Você quer auditar os resultados ? (S/N) ");
+        string audit = Console.ReadLine() ?? "N";
+        audit = audit.ToUpper();
 
-    string audit = Console.ReadLine() ?? "N";
-    audit = audit.ToUpper();
+            if (audit == "S")
+            {
+            Console.WriteLine("Your log will be saved into ElectronicUrn/log.txt");
+            string filePath = Directory.GetCurrentDirectory();
 
-    if (audit == "S")
-    {
-    Console.WriteLine("Your log will be saved into CalcMachine/log.txt");
-    string filePath = Directory.GetCurrentDirectory();
+            Console.WriteLine(filePath);
 
-    FileStream filestream = new FileStream("log.txt", FileMode.Create);
-    var streamwriter = new StreamWriter(filestream);
-    streamwriter.AutoFlush = true;
-    Console.SetOut(streamwriter);
-    Console.SetError(streamwriter);
-    }
+            File.Create(filePath + @"\" + "log.txt");
+            var streamwriter = new StreamWriter(filePath + @"\log.txt");
+            streamwriter.AutoFlush = true;
+            Console.SetOut(streamwriter);
+            Console.SetError(streamwriter);
+            }
+ */       
+    
+}
